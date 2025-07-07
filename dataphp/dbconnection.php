5ac91656e9fee2e4b945cac15401e41105d2e691
Aguiar012 <113046681+Aguiar012@users.noreply.github.com>
2025-07-07 09:58:52 -0300

    troquei robux_inscricoes por robux_inscricao

--- dataphp/dbconnection.php
+++ dataphp/dbconnection.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $user = 'root';
 $pass = '';
-$dbname = 'robux_inscricoes';
+$dbname = 'robux_inscricao';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
